5b18028bfdd8d2c81cb50e02a94fc301cb01442f
LAPTOP-AMAZON <rupamrouth.it@gmail.com>
2020-01-25 12:36:16 +0530

    Framework uploaded

--- src/test/java/com/bddtestng/runner/TestRunner.java
+++ src/test/java/com/bddtestng/runner/TestRunner.java
@@ -2,19 +2,47 @@ package com.bddtestng.runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
 
-@RunWith(Cucumber.class)
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		
 		features="D:\\Projects_Selenium\\BDD-TESTNG\\src\\main\\java\\com\\bddtestng\\feature",
 		glue= {"com\\bddtestng\\stepdefinitions"},
-		plugin= {"pretty", "html:test-output"},
+		plugin= {"pretty", "html:test-output", "html:target/cucumber-reports/cucumber-pretty"},
 		dryRun=false,
 		monochrome=true,
 		strict=true,
-		tags= {})
+		tags= {"@SmokeTest"})
 
 public class TestRunner {
 
+	private TestNGCucumberRunner testNGCucumberRunner;
+	
+	@BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+	
+	@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+	
+	@DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+	
+	@AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
 }
